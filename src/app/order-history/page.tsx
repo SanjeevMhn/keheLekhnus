db39328a0e36c76d13027760f8ecc65e1dd3e37b
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-03-13 15:40:01 +0545

    [FIXED & ADDED]: fixed order history not loading on page refresh, added order cancel functionality for the user

--- src/app/order-history/page.tsx
+++ src/app/order-history/page.tsx
@@ -8,6 +8,7 @@ import BreadCrumb, { TBreadCrumb } from "../(components)/breadcrumb";
 import DataTable, { Columns, PagerConfig } from "../(components)/data-table";
 import Link from "next/link";
 import { showConfirm } from "../lib/confirmation/confirmationSlice";
+import { showNotification } from "../lib/notifications/notificationSlice";
 
 export default function Page() {
 
@@ -16,17 +17,15 @@ export default function Page() {
   const [orders, setOrders] = useState<Array<any>>([]);
 
   useEffect(() => {
-    if (!orderHistoryFetched.current) {
+    if (authUser.user_info !== null) {
       getOrderHistory();
     }
-    return () => {
-      orderHistoryFetched.current = true;
-    }
-  })
+
+  }, [authUser.user_info])
 
   const getOrderHistory = async () => {
     try {
-      const response = await api.get(`${process.env.NEXT_PUBLIC_API_URL}/user/${authUser.user_info?.user_id}`);
+      const response = await api.get(`${process.env.NEXT_PUBLIC_API_URL}/user/order-history/${authUser.user_info?.user_id}`);
       const data = await response.data;
       setOrders(data.orders);
     } catch (err: any) {
@@ -107,11 +106,24 @@ export default function Page() {
   ]
 
   const dispatch = useDispatch<any>();
-  const handleChangeStatus = async () => {
+  const handleCancelOrder = (orderId: any) => {
     dispatch(showConfirm({
       message: 'Do you want to cancel this order?',
-      onConfirm: () => {
-
+      onConfirm: async () => {
+        try {
+          const response = await api.patch(`${process.env.NEXT_PUBLIC_API_URL}/orders/id/${orderId}`,{
+            order_status: 'CANCELLED'
+          });
+          if (response.status == 200) {
+            dispatch(showNotification({
+              message: 'Order cancelled successfully',
+              type: 'success'
+            }))
+            getOrderHistory();
+          }
+        } catch (err: any) {
+          console.error(err)
+        }
       }
     }))
   }
@@ -145,7 +157,7 @@ export default function Page() {
                     </div>
                     {
                       ord.order_status === 'PENDING' ? (
-                        <button className="icon-container cancel-order" onClick={handleChangeStatus}>
+                        <button className="icon-container cancel-order" onClick={() => handleCancelOrder(ord.order_id)}>
                           <svg xmlns="http://www.w3.org/2000/svg" height="22" width="22" viewBox="0 0 448 512">
                             <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
                           </svg>
