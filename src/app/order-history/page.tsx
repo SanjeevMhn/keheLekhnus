37db0941ade1ac18f202af48ef156e016bc4d11c
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-03-14 18:30:53 +0545

    [ADDED]: added tabs for for seperating orders based on their status

--- src/app/order-history/page.tsx
+++ src/app/order-history/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef, useState } from "react"
+import { FC, useEffect, useRef, useState } from "react"
 import api from "../service/interceptor/interceptor";
 import { TAuthState } from "../lib/auth/authSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,15 +9,18 @@ import DataTable, { Columns, PagerConfig } from "../(components)/data-table";
 import Link from "next/link";
 import { showConfirm } from "../lib/confirmation/confirmationSlice";
 import { showNotification } from "../lib/notifications/notificationSlice";
-import { DialogState, showDialog } from "../lib/dialog/dialogSlice";
-import OrderItemDetail from "../(components)/order-item-detail";
 
 export default function Page() {
 
-  const orderHistoryFetched = useRef<boolean>(false);
   const authUser: TAuthState = useSelector((state: any) => state.auth);
   const [orders, setOrders] = useState<Array<any>>([]);
-  const dialogState: DialogState = useSelector((state: any) => state.dialog);
+  const [tabActive, setTabActive] = useState<string>('all');
+  const [tabItems, setTabItems] = useState<Array<string>>([
+    'all',
+    'pending',
+    'completed',
+    'cancelled'
+  ])
 
   useEffect(() => {
     if (authUser.user_info !== null) {
@@ -180,6 +183,59 @@ export default function Page() {
     }))
   }*/
 
+  const OrderHistoryItem: FC<{ ord: any }> = ({ ord }) => {
+    return (
+      <article className={`order-history-item ${orderActive == ord.order_id ? 'active' : ''}`}>
+        <div className="order-history-details p-[10px] text-[17px]">
+          <div className="order-number">
+            <span className="font-semibold">Order</span>: &nbsp;{ord.order_number}
+          </div>
+          <div className="order-date">
+            <span className="font-semibold">Order Date</span>: &nbsp;{ord.order_date}
+          </div>
+
+          {
+            orderActive == ord.order_id && orderItems.length !== 0 ? (
+              <div className="order-total">
+                <span className="font-semibold">Order Total</span>: &nbsp;{orderItems.reduce((previous: any, it: any) => previous + (it.prod_price * it.prod_quantity), 0)}
+              </div>
+            ) : null
+          }
+          {
+            ord.order_status === 'PENDING' ? (
+              <button className="icon-container cancel-order" onClick={() => handleCancelOrder(ord.order_id)}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="22" width="22" viewBox="0 0 448 512">
+                  <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
+                </svg>
+              </button>
+            ) : null
+          }
+          <div className={`status ${ord.order_status}`}>{ord.order_status}</div>
+          <button className="show-table icon-container" onClick={() => handleGetOrderData(ord.order_id)}>
+            {
+              orderActive == ord.order_id ? (
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" /></svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" /></svg>
+              )
+            }
+          </button>
+        </div>
+        {
+          orderActive == ord.order_id ? (
+            <DataTable
+              title={'Order Items'}
+              data={orderItems}
+              pagerConfig={pagerConfig}
+              columns={gridCol}
+              showActionCol={false}
+            />
+          ) : null
+        }
+      </article>
+    )
+  }
+
 
   return (
     <div className="main-wrapper px-5 lg:px-0">
@@ -198,60 +254,37 @@ export default function Page() {
             </Link>
             <p className="info-text-sm px-4 text-center text-base">(Cancelled orders will be deleted from orders history)</p>
           </div>) : (
-          <section className="pt-[15px] order-history-container">
-            {
-              orders.map((ord: any, index: any) => (
-                <article className={`order-history-item ${orderActive == ord.order_id ? 'active' : ''}`} key={index}>
-                  <div className="order-history-details p-[10px] text-[17px]">
-                    <div className="order-number">
-                      <span className="font-semibold">Order</span>: &nbsp;{ord.order_number}
-                    </div>
-                    <div className="order-date">
-                      <span className="font-semibold">Order Date</span>: &nbsp;{ord.order_date}
-                    </div>
+          <>
+            <div className="tab-container pb-[5px] flex flex-wrap gap-[12px] text-[18px]">
+              {
+                tabItems.map((ti: string, index: number) => (
+                  <button
+                    type="button"
+                    className={`capitalize leading-none ${tabActive === ti ? 'btn-primary !min-w-[65px] !p-[5px_10px]' : ''}`}
+                    key={index}
+                    onClick={() => {
+                      setTabActive(ti);
+                      setOrderActive(null);
+                      setOrderItems([])
+                    }}>{ti}</button>
+                ))
+              }
 
-                    {
-                      orderItems.length !== 0 ? (
-                        <div className="order-total">
-                          <span className="font-semibold">Order Total</span>: &nbsp;{orderItems.reduce((previous: any, it: any) => previous + (it.prod_price * it.prod_quantity), 0)}
-                        </div>
-                      ) : null
-                    }
-                    {
-                      ord.order_status === 'PENDING' ? (
-                        <button className="icon-container cancel-order" onClick={() => handleCancelOrder(ord.order_id)}>
-                          <svg xmlns="http://www.w3.org/2000/svg" height="22" width="22" viewBox="0 0 448 512">
-                            <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
-                          </svg>
-                        </button>
-                      ) : null
-                    }
-                    <div className={`status ${ord.order_status}`}>{ord.order_status}</div>
-                    <button className="show-table icon-container" onClick={() => handleGetOrderData(ord.order_id)}>
-                      {
-                        orderActive == ord.order_id ? (
-                          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" /></svg>
-                        ) : (
-                          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" /></svg>
-                        )
-                      }
-                    </button>
-                  </div>
-                  {
-                    orderActive == ord.order_id ? (
-                      <DataTable
-                        title={'Order Items'}
-                        data={orderItems}
-                        pagerConfig={pagerConfig}
-                        columns={gridCol}
-                        showActionCol={false}
-                      />
-                    ) : null
-                  }
-                </article>
-              ))
-            }
-          </section>
+            </div>
+            <section className="pt-[15px] order-history-container">
+              {
+                tabActive !== 'all'
+                ? orders.filter((ord: any) => ord.order_status.toLowerCase() === tabActive)
+                  .map((fil: any, index: any) => (
+                    <OrderHistoryItem ord={fil} key={index} />
+                  ))
+                : orders.map((ord: any, index: any) => (
+                  <OrderHistoryItem ord={ord} key={index} />
+                ))
+              }
+
+            </section>
+          </>
         )
       }
     </div>
